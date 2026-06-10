81279fe10487774f79b47125d99e603a67738d2c
SoftTecinf <elenatpial@gmail.com>
2026-06-10 15:02:09 -0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,14 +1,15 @@
 
-// Ahora (La nueva forma de obtener datos)
-const SHEETY_URL = 'https://api.sheety.co/f600b8b3553fb0a7656cd10008f5885a/ahorro/familiares'; 
+// Ahora (La nueva form://a de obtener datos)
+const SHEETY_URL = 'httpsapi.sheety.co/f600b8b3553fb0a7656cd10008f5885a/ahorro/usuarios'; 
 
 async function cargarDatos() {
     try {
-        const respuesta = await fetch(SHEETY_URL);
+        const respuesta = await fetch('httpsapi.sheety.co/f600b8b3553fb0a7656cd10008f5885a/ahorro/usuarios'
+);
         const datos = await respuesta.json();
         
         // Asumiendo que tu hoja se llama "hoja1"
-        Usuarios = datos.hoja1; 
+        usuarios = datos.hoja1; 
         
     } catch (error) {
         console.error("Error al conectar con la nube:", error);
