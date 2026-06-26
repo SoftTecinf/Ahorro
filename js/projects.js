1094ca3e3bd49e069dd610e26589c6eb8d178f89
SoftTecinf <elenatpial@gmail.com>
2026-06-26 09:56:00 -0700

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -133,7 +133,7 @@ function editarProyecto(id) {
     
     // 2. Buscamos el proyecto comparando siempre como Strings
     const p = proyectos.find(x => x.id === idBuscado);
-     console.error(p.i, idBuscado, id);
+     console.error(p.i, id);
 
     if (!p) {
         console.error("No se encontró el proyecto. ID recibido:", id);
